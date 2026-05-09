0377c7a26f11430eb814e04eb45a14ef06fa1555
shashikant <kant19.shashi@gmail.com>
2026-05-09 10:01:24 +0530

    prgram file and created folder

---
+++ chapter_02_basic/01_greater_number.js
@@ -0,0 +1,10 @@
+let a = 25;
+let b = 40;
+
+if (a > b) {
+    console.log(a + " is greater than " + b);
+} else if (b > a) {
+    console.log(b + " is greater than " + a);
+} else {
+    console.log("Both numbers are equal");
+}
